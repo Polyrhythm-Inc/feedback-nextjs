ea71e115d9c0db990e2081443dcc55bf4dcfb78b
Hitoshi Yunoki <yunoki@polyrhythm.jp>
2025-07-09 11:33:20 +0900

    Fix migration to safely convert timestamps from BigInt to Integer

--- prisma/migrations/20250626111252_change_timestamp_to_int/migration.sql
+++ prisma/migrations/20250626111252_change_timestamp_to_int/migration.sql
@@ -1,12 +1,32 @@
 /*
-  Warnings:
+Warnings:
 
-  - You are about to alter the column `timestamp` on the `feedbacks` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Integer`.
-  - You are about to alter the column `timestamp` on the `screenshot_data` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Integer`.
+- You are about to alter the column `timestamp` on the `feedbacks` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Integer`.
+- You are about to alter the column `timestamp` on the `screenshot_data` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Integer`.
 
 */
--- AlterTable
-ALTER TABLE "feedbacks" ALTER COLUMN "timestamp" SET DATA TYPE INTEGER;
+
+-- まず、ミリ秒単位のタイムスタンプを秒単位に変換
+UPDATE "feedbacks"
+SET
+    "timestamp" = "timestamp" / 1000
+WHERE
+    "timestamp" > 2147483647;
+
+UPDATE "screenshot_data"
+SET
+    "timestamp" = "timestamp" / 1000
+WHERE
+    "timestamp" > 2147483647;
 
 -- AlterTable
-ALTER TABLE "screenshot_data" ALTER COLUMN "timestamp" SET DATA TYPE INTEGER;
+ALTER TABLE "feedbacks"
+ALTER COLUMN "timestamp"
+SET
+    DATA TYPE INTEGER;
+
+-- AlterTable
+ALTER TABLE "screenshot_data"
+ALTER COLUMN "timestamp"
+SET
+    DATA TYPE INTEGER;
